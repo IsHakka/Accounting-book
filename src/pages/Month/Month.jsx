9a03ec316b50a605b17f6a2122b61f585ece6fdc
Xian <qaq81704@gmail.com>
2023-10-22 10:08:37 +0800

    feat:當前月統計計算

--- src/pages/Month/Month.jsx
+++ src/pages/Month/Month.jsx
@@ -9,15 +9,35 @@ import _ from 'lodash';
 const Month = () => {
     // 獲得數據
     const { billList } = useSelector(state => state.bill);
-    const monthGroup =  useMemo(() => {
+    const monthGroup = useMemo(() => {
         // 按月做數據分組
-        return _.groupBy(billList,item=> dayjs(item.date).format('YYYY-MM'))
+        return _.groupBy(billList, item => dayjs(item.date).format('YYYY-MM'))
     }, [billList])
-    console.log(monthGroup);
     // 談框開關
     const [dateVisible, setDateVisible] = useState(false);
     // 時間顯示
     const [currentDate, setCurrentDate] = useState(() => dayjs(new Date()).format('YYYY-MM'))
+
+    const [currentMonthList, setMonthList] = useState([])
+
+    // 計算收入、支出、餘額
+    const monthResult = useMemo(() => {
+        const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, b) => a + b.money, 0)
+        const income = currentMonthList.filter(item => item.type === 'income').reduce((a, b) => a + b.money, 0)
+
+        return {
+            pay,
+            income,
+            total: pay + income
+        }
+
+    }, [currentMonthList])
+    const onConfirm = (date) => {
+        setDateVisible(false);
+        const formatDate = dayjs(date).format('YYYY-MM');
+        setMonthList(monthGroup[formatDate])
+        setCurrentDate(formatDate);
+    }
 
     return (
         <div className="monthlyBill">
@@ -36,15 +56,15 @@ const Month = () => {
                     {/* 統計區域 */}
                     <div className='twoLineOverview'>
                         <div className="item">
-                            <span className="money">{100}</span>
-                            <span className="type">支出</span>
-                        </div>
-                        <div className="item">
-                            <span className="money">{200}</span>
+                            <span className="money">{monthResult.income}</span>
                             <span className="type">收入</span>
                         </div>
                         <div className="item">
-                            <span className="money">{200}</span>
+                            <span className="money">{monthResult.pay}</span>
+                            <span className="type">支出</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{monthResult.total}</span>
                             <span className="type">餘額</span>
                         </div>
                     </div>
@@ -56,7 +76,7 @@ const Month = () => {
                         visible={dateVisible}
                         max={new Date()}
                         onCancel={() => setDateVisible(false)}
-                        onConfirm={(date) => { setDateVisible(false); const formatDate = dayjs(date).format('YYYY-MM'); setCurrentDate(formatDate); }}
+                        onConfirm={onConfirm}
                         onClose={() => setDateVisible(false)}
                     />
                 </div>
